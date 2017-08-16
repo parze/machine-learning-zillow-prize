af137168d071d3e62696f7ac6d59201c08ffd682
Par Steffansson <par.steffansson@sent.com>
2017-08-16 11:41:06 +0200

    Removed bagging_fraction when it improved the result a small bit

--- src/kernel.py
+++ src/kernel.py
@@ -64,7 +64,6 @@ def train_lightgbm(x, y):
         'boosting_type': 'gbdt',
         'objective': 'regression',
         'metric': 'l1',
-        'bagging_fraction': 0.85,
         'bagging_freq': 40,
         'num_leaves': 512,
         'min_data': 500,
